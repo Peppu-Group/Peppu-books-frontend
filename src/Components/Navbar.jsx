5633a115ea967d83f54ed6df8f1b7a49375e8ed6
Bishopukpai <bishopukpai0@gmail.com>
2022-12-19 04:26:15 +0100

    changed the divitists in nav bar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,17 +7,17 @@ const Navbar = () => {
         {/*Home page Nave-bar components style={{width: 74, height: 30, marginTop: 13}} */}
       <section className='home-navbar'>
         <figure className='logo'> 
-           <img src={IMG_2437}/>
-         </figure>
-         <article className='nav-menu'>
-           <a href="/"><li>Home</li></a>
-           <a href="/features"> <li>Features</li></a>
-           <a href='/products'> <li>Products</li></a>
+          <img src={IMG_2437}/>
+        </figure>
+        <article className='nav-menu'>
+          <a href="/"><li>Home</li></a>
+          <a href="/features"> <li>Features</li></a>
+          <a href='/products'> <li>Products</li></a>
            <a href='/pricing'><li>Pricing</li></a>
            <a href='/about'><li>About&nbsp;us</li></a>
-          <figure className='search-icon'>
-            <AiOutlineSearch />
-          </figure>
+        <figure className='search-icon'>
+          <AiOutlineSearch />
+        </figure>
         <a href='/getstarted'><li><button>Get&nbsp;Started</button></li></a>
       </article>
      

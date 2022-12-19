fe6de51da2c29d7c0a778c8fdd5f5cdc375c07a1
Bishopukpai <bishopukpai0@gmail.com>
2022-12-19 04:23:28 +0100

    removed all the divitists from the navbar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,27 +3,27 @@ import IMG_2437 from '../assets/IMG_2437.png'
 import {AiOutlineSearch } from 'react-icons/ai'
 const Navbar = () => {
   return (
-    <div>
+    <main>
         {/*Home page Nave-bar components style={{width: 74, height: 30, marginTop: 13}} */}
-      <div className='home-navbar'>
-       <div className='logo'> 
-        <img src={IMG_2437}/>
-        </div>
-        <div className='nav-menu'>
-         <a href="/"><li>Home</li></a>
-         <a href="/features"> <li>Features</li></a>
-         <a href='/products'> <li>Products</li></a>
-         <a href='/pricing'><li>Pricing</li></a>
-         <a href='/about'><li>About&nbsp;us</li></a>
-        <div className='search-icon'>
-        <AiOutlineSearch />
-        </div>
+      <section className='home-navbar'>
+        <figure className='logo'> 
+           <img src={IMG_2437}/>
+         </figure>
+         <article className='nav-menu'>
+           <a href="/"><li>Home</li></a>
+           <a href="/features"> <li>Features</li></a>
+           <a href='/products'> <li>Products</li></a>
+           <a href='/pricing'><li>Pricing</li></a>
+           <a href='/about'><li>About&nbsp;us</li></a>
+          <figure className='search-icon'>
+            <AiOutlineSearch />
+          </figure>
         <a href='/getstarted'><li><button>Get&nbsp;Started</button></li></a>
-      </div>
+      </article>
      
-      </div>
+      </section>
     
-    </div>
+    </main>
   )
 }
 

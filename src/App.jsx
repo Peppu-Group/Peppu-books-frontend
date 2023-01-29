0ac4d07e1b90f396043854fd9b22a0c5128ce444
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-01-29 16:26:29 +0100

    bootstrapping privacy page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Products from './Pages/Products'
 import Pricing from './Pages/Pricing'
 import About from './Pages/About'
 import Features from './Pages/Features'
+import Privacy from './Pages/Privacy'
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/pricing" element={<Pricing/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/features" element={<Features/>} />
+          <Route path="/privacy" element={<Privacy/>} />
       </Routes>
       </Router>
     </div>
---
+++ src/Pages/Privacy.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Privacy = () => {
+    return (
+        <main class="root"></main>
+    )}
+export default Privacy

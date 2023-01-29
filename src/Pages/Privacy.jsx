1e2a3a0248768aecec86cffb7e8a1f101eb1ae2c
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-01-29 16:45:13 +0100

    hero section ready

--- src/Pages/Privacy.jsx
+++ src/Pages/Privacy.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import Container from 'react-bootstrap/Container';
+
 
 const Privacy = () => {
     return (
-        <main class="root"></main>
-    )}
+        <main class="root">
+            <nav className='fixed'>
+                <Navbar />
+            </nav>
+            <div className='privacy-section'>
+                <Container fluid >
+                    <div className='body-header'>
+                        <p className='header'>Privacy</p>
+                        <p>Policy</p>
+                    </div>
+                    <div className='first-paragragh'>
+                        <p>We take your privacy seriously. We do not share or sell your information with anybody.</p>
+                    </div>
+                </Container>
+            </div>
+            
+            
+        </main>
+    )
+}
 export default Privacy

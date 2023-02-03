c881903cc20cb2db0f714c52987bed0cb6a5eae6
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-02-03 06:25:20 +0100

    make sure all buttons are functional

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -183,27 +183,17 @@ const Homepage = () => {
                 <div className="col-md-3 mb-md-0 mb-3">
                   <ul className="">
                     <li>Quick Links</li>
-                    <li><a href="#!">About</a></li>
-                    <li><a href="#!">Services</a></li>
-                    <li><a href="#!">Products</a></li>
-                    <li><a href="#!">Contact Us</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/privacy">Privacy Policies</a></li>
+                    <li><a href="/terms">Terms & Conditions</a></li>
                   </ul>
                 </div>
 
                 <div className="col-md-3 mb-md-0 mb-3">
                   <ul className="">
                     <li>Follow Us</li>
-                    <li><a href="#!">Twitter</a></li>
-                    <li><a href="#!">Instagram</a></li>
-                    <li><a href="#!">Facebook</a></li>
-                  </ul>
-                </div>
-
-                <div className="col-md-3 mb-md-0 mb-3">
-                  <ul className="">
-                    <li>Terms & Policies</li>
-                    <li><a href="/privacy">Privacy Policies</a></li>
-                    <li><a href="/terms">Terms & Conditions</a></li>
+                    <li><a href="https://twitter.com/hannydevelop">Twitter</a></li>
+                    <li><a href="https://github.com/Peppu-Group">Github</a></li>
                   </ul>
                 </div>
               </div>

4359e5d6f0e5a0c76fbf314c6c0f27825200b75d
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-03-23 07:31:44 +0100

    invoice in progress

--- src/Pages/Invoice.jsx
+++ src/Pages/Invoice.jsx
@@ -1,58 +1,9 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { Page, Image, PDFViewer, Text, View, Font, Document, StyleSheet } from '@react-pdf/renderer';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import TextareaAutosize from 'react-textarea-autosize'
 
-const styles = StyleSheet.create({
-    page: { backgroundColor: 'tomato' },
-    section: { color: 'white', textAlign: 'center', margin: 30 },
-    body: {
-        paddingTop: 35,
-        paddingBottom: 65,
-        paddingHorizontal: 35,
-    },
-    title: {
-        fontSize: 24,
-        textAlign: 'center',
-        fontFamily: 'Oswald',
-    },
-    author: {
-        fontSize: 12,
-        textAlign: 'center',
-        marginBottom: 40,
-    },
-    subtitle: {
-        fontSize: 18,
-        margin: 12,
-        fontFamily: 'Oswald',
-    },
-    text: {
-        margin: 12,
-        fontSize: 14,
-        textAlign: 'justify',
-        fontFamily: 'Times-Roman',
-    },
-    image: {
-        marginVertical: 15,
-        marginHorizontal: 100,
-    },
-    header: {
-        fontSize: 12,
-        marginBottom: 20,
-        textAlign: 'center',
-        color: 'grey',
-    },
-    pageNumber: {
-        position: 'absolute',
-        fontSize: 12,
-        bottom: 30,
-        left: 0,
-        right: 0,
-        textAlign: 'center',
-        color: 'grey',
-    },
-});
 
 Font.register({
     family: 'Oswald',
@@ -65,36 +16,128 @@ const Subtitle = ({ children, ...props }) => (
     </Text>
 );
 
-// Create Document Component
-const MyDocument = () => (
-    <Document>
-        <Page size="A4" style={styles.page}>
-            <View style={styles.section}>
-                <Text style={styles.header} fixed>
-                    ~ Created with react-pdf ~
-                </Text>
-                <Text style={styles.title}>Don Quijote de la Mancha</Text>
-      <Text style={styles.author}>Miguel de Cervantes</Text>
-      <Image
-        style={styles.image}
-        src="http://static.donquijote.org/images/blogs/dq-reg/don-quijote-de-la-mancha.jpg"
-      />
-      <Subtitle>
-        Capítulo I: Que trata de la condición y ejercicio del famoso hidalgo D.
-        Quijote de la Mancha
-      </Subtitle>
-            </View>
-        </Page>
-    </Document>
-);
+
+
+const TextArea = () => {
+    const [isHover, setIsHover] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHover(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHover(false);
+    };
+
+    const onInputChange = (e) => {
+        return e.target.value;
+    };
+
+    const boxStyle = {
+        resize: 'none',
+        background: '#FFFFFF',
+        border: 0,
+        backgroundColor: isHover ? 'lightblue' : 'white',
+        resize: 'none',
+        margin: '0 0 0 50px'
+    };
+    return (
+        <TextareaAutosize
+            maxRows={4}
+            placeholder="Your Company"
+            style={boxStyle}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        />
+    );
+
+}
 
 const Invoice = () => {
+
+    const [isHover, setIsHover] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHover(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHover(false);
+    };
+
+    const onInputChange = (e) => {
+        return e.target.value;
+    };
+
+    const boxStyle = {
+        resize: 'none',
+        background: '#FFFFFF',
+        border: 0,
+        backgroundColor: isHover ? 'lightblue' : 'white',
+        resize: 'none',
+        margin: '0 0 0 50px'
+    };
+
     return (
+        <body className='inv-none'>
+            <div className='page'>
+                <div className='inv-logo'>
+                    Logo
+                </div>
+                <div className='comp-name style'>
+                    <TextareaAutosize
+                        maxRows={4}
+                        placeholder="Your Company"
+                        style={boxStyle}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                    />
 
-        <PDFViewer>
-            <MyDocument />
-        </PDFViewer>
+                    <TextareaAutosize
+                        maxRows={4}
+                        className='textAreaWithoutStyle'
+                        value='INVOICE'
+                        placeholder="Invoice"
+                        style={boxStyle}
+                    />
+                </div>
 
+                <div className='comp-name'>
+                    <TextareaAutosize
+                        maxRows={4}
+                        className='textAreaWithoutStyle'
+                        placeholder="Your Name"
+                        style={boxStyle}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                    />
+                </div>
+
+                <div className='comp-name'>
+                    <TextareaAutosize
+                        maxRows={4}
+                        className='textAreaWithoutStyle'
+                        placeholder="Company's Address"
+                    />
+                </div>
+
+                <div className='comp-name'>
+                    <TextareaAutosize
+                        maxRows={4}
+                        className='textAreaWithoutStyle'
+                        placeholder="City, Zip code"
+                    />
+                </div>
+
+                <div className='comp-name'>
+                    <TextareaAutosize
+                        maxRows={4}
+                        className='textAreaWithoutStyle'
+                        placeholder="Country"
+                    />
+                </div>
+            </div>
+        </body>
 
     )
 }
